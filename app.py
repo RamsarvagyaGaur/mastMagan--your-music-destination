c3dac0b3c936a8fdeb97bce2790e660a1dc841cd
Ram Sarvagya Gaur <150837985+RamsarvagyaGaur@users.noreply.github.com>
2024-05-10 04:05:08 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,53 @@
+from flask import Flask,request,render_template
+import numpy as np
+import pandas as pd
+import pickle
+
+# laoding models
+df = pickle.load(open('df.pkl','rb'))
+similarity = pickle.load(open('similarity.pkl','rb'))
+
+
+def recommendation(song_df):
+    idx = df[df['song'] == song_df].index[0]
+    distances = sorted(list(enumerate(similarity[idx])), reverse=True, key=lambda x: x[1])
+
+    songs = []
+    for m_id in distances[1:21]:
+        songs.append(df.iloc[m_id[0]].song)
+
+    return songs
+
+
+# flask app
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    names = list(df['song'].values)
+    return render_template('index.html',name = names)
+@app.route('/recom',methods=['POST'])
+def mysong():
+    user_song = request.form['names']
+    songs = recommendation(user_song)
+
+    return render_template('index.html',songs=songs)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/Login')
+def login():
+    return render_template('login.html')
+
+@app.route('/Signup')
+def signup():
+    return render_template('signup.html')
+
+
+
+
+# python
+if __name__ == "__main__":
+    app.run(debug=True)
